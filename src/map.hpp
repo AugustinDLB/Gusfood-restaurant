bae6155541cd607e23344b27dd1a56191407b6d7
AugustinDLB <98012937+AugustinDLB@users.noreply.github.com>
2026-05-05 15:22:23 +0200

    gitignore

--- src/map.hpp
+++ src/map.hpp
@@ -24,7 +24,6 @@ class Map {
         bool is_out_of_map(const pos_x_t &x, const pos_y_t &y);
         void set_area(const pos_x_t& x, const pos_y_t& y, const obj_size_t& sz, const MapState map_state);
         void set_area(const pos_x_t &pos_x1, const pos_y_t &pos_y1,const pos_x_t &pos_x2, const pos_y_t &pos_y2, const MapState& state);
-
 };
 
 #endif
